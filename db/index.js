ce27e0168d4b9b54cc69d435b2c25e33518019d2
Jane Kavanagh <82611639+JaneTrish@users.noreply.github.com>
2022-03-21 19:24:01 +0200

    db config

--- db/index.js
+++ db/index.js
@@ -1,33 +1,21 @@
-require('dotenv').config();
 const { Pool, Client } = require('pg');
-
+const connectionString =
+  'postgres://daersurxxnmcex:e0da560f0026e165dc1c47afb019575a2194adf0e6d58c29d367b4e7b577d649@ec2-3-231-254-204.compute-1.amazonaws.com:5432/d4i8p44fnsafk';
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString,
 });
-
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString,
 });
-
 client.connect();
-
-client.query(
-  'SELECT table_schema,table_name FROM information_schema.tables;',
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  client.end();
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
