a31dfadcb1bef62c9539690c1a9995487c9d0fe9
Jane Kavanagh <82611639+JaneTrish@users.noreply.github.com>
2022-03-21 20:30:23 +0200

    db config again

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const connectionString =
   'postgres://daersurxxnmcex:e0da560f0026e165dc1c47afb019575a2194adf0e6d58c29d367b4e7b577d649@ec2-3-231-254-204.compute-1.amazonaws.com:5432/d4i8p44fnsafk';
 const pool = new Pool({
   connectionString,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
 pool.query('SELECT NOW()', (err, res) => {
   console.log(err, res);
@@ -12,7 +12,7 @@ pool.query('SELECT NOW()', (err, res) => {
 
 const client = new Client({
   connectionString,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
 client.connect();
 client.query('SELECT NOW()', (err, res) => {
